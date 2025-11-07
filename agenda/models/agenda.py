5a7899da87000f7deb33b6bb03b75b94a78f1195
eudesgomesss <eudes.souza@spassu.com.br>
2025-11-07 08:46:23 -0300

    feat(137125): Adiciona back-end para agendar o processo de convocação

--- agenda/models/agenda.py
+++ agenda/models/agenda.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from auditlog.registry import auditlog
 
 from .base import BaseModel
+from .constants import MODALIDADE_CHOICES
 
 
 class Agenda(BaseModel):
@@ -17,10 +18,55 @@ class Agenda(BaseModel):
     cargo_nome = models.CharField(max_length=200, verbose_name="Nome do Cargo")
     data_escolha = models.DateTimeField(verbose_name="Data de Publicação", default=timezone.now)
 
+    modalidade = models.CharField(
+        max_length=20,
+        choices=MODALIDADE_CHOICES,
+        verbose_name="Modalidade",
+        blank=True,
+        null=True
+    )
+    escolha_em = models.DateField(
+        verbose_name="Data de Escolha",
+        blank=True,
+        null=True
+    )
+    nomeacao_em = models.DateField(
+        verbose_name="Data de Nomeação",
+        blank=True,
+        null=True
+    )
+    classificacao = models.IntegerField(
+        verbose_name="Classificação",
+        blank=True,
+        null=True
+    )
+    sessao = models.CharField(
+        max_length=200,
+        verbose_name="Sessão",
+        blank=True,
+        null=True
+    )
+    retardatario = models.BooleanField(
+        verbose_name="Retardatário",
+        default=False,
+        blank=True,
+        null=True
+    )
+    hora_convocacao_inicio = models.TimeField(
+        verbose_name="Hora de Início da Convocação",
+        blank=True,
+        null=True
+    )
+    hora_convocacao_fim = models.TimeField(
+        verbose_name="Hora de Fim da Convocação",
+        blank=True,
+        null=True
+    )
+
     class Meta:
         verbose_name = "Agenda de Convocação"
         verbose_name_plural = "Agendas de Convocação"
-        ordering = ['-criado_em']
+        ordering = ['escolha_em', 'hora_convocacao_inicio']
         db_table = 'agenda'
     
     def __str__(self):
